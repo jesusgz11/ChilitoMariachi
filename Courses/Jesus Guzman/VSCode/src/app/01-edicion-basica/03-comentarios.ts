56a0fe6404c42ecccb18f8674650d2453e3eed35
Jesus Guzman <jesusguzman.0311@gmail.com>
2020-05-09 16:31:40 -0500

    Comments

--- Courses/Jesus Guzman/VSCode/src/app/01-edicion-basica/03-comentarios.ts
+++ Courses/Jesus Guzman/VSCode/src/app/01-edicion-basica/03-comentarios.ts
@@ -6,19 +6,18 @@
         ⌘ /
         
         Ctrl + \
+    Change your key shortuts im using Ctrl + / and Alt + /
 */
 
 const a = 10;
 const b = 20;
 const c = { a, b };
 
-
-const a = 30;
+/* const a = 30;
 const b = 40;
-const c = { a, b };
+const c = { a, b }; */
 
-
-console.log( a, b, c );
+console.log(a, b, c);
 
 // Resultado final
 // Sólo dejar un bloque comentado
